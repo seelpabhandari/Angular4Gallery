bdf6ba66d497ce294036de36298e8d6a2ffe2106
Shilpa Bhandari <shilpa.bhandari@zurelsoft.com>
2019-01-08 23:38:05 +0545

    gallery app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AlertModule } from 'ngx-bootstrap/alert'; 
+import { AlertModule } from 'ngx-bootstrap/alert';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ImageComponent } from './image/image.component';
+import { ImageService } from './image/shared/image.service';
+import { ImageFilterPipe } from './image/shared/filter.pipe';
+import {appRoutes} from '../routes';
+import { RouterModule } from '../../node_modules/@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    GalleryComponent
+    GalleryComponent,
+    ImageComponent,
+    ImageFilterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ImageService, ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/image/shared/image.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  visibleImages = [];
+  getImages() {
+    return this.visibleImages = IMAGES.slice(0);
+  }
+
+  getImage(id: number) {
+    return IMAGES.slice(0).find(Image => Image.id == id);
+  }
+}
+
+const IMAGES = [
+  { "id": 0, "category": "Boats", "caption": "view for a boat", "url": "assets/img/img_01.jpg" },
+  { "id": 1, "category": "Boats", "caption": "view for a boat", "url": "assets/img/img_02.jpg" },
+  { "id": 2, "category": "Boats", "caption": "view for a boat", "url": "assets/img/img_03.jpg" },
+  { "id": 3, "category": "Boats", "caption": "view for a boat", "url": "assets/img/img_04.jpg" },
+  { "id": 4, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_05.jpg" },
+  { "id": 5, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_06.jpg" },
+  { "id": 6, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_07.jpg" },
+  { "id": 7, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_08.jpg" },
+  { "id": 8, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_09.jpg" },
+  { "id": 9, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_10.jpg" },
+  { "id": 10, "category": "Camping", "caption": "view for a boat", "url": "assets/img/img_11.jpg" },
+  { "id": 11, "category": "library", "caption": "view for a boat", "url": "assets/img/img_12.jpg" },
+  { "id": 12, "category": "Library", "caption": "view for a boat", "url": "assets/img/img_13.jpg" }
+]
